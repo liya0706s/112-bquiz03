dc8e6909a325d726e19a247d84d3e406f02110c9
Angie <liya0706s@gmail.com>
2024-01-19 15:27:06 +0800

    完成訂單管理中的快速刪除功能

--- back/order.php
+++ back/order.php
@@ -1,9 +1,10 @@
 <h2 class="ct">訂單清單</h2>
 <div class="qdel">
     快速刪除：
-    <input type="radio" name="type" value='1' checked>依日期
+    <!-- name="type" value是date和movie -->
+    <input type="radio" name="type" value='date' checked>依日期
     <input type="text" name="date" id="date">
-    <input type="radio" name="type" value="2">依電影
+    <input type="radio" name="type" value="movie">依電影
     <select name="movie" id="movie">
         <?php
         $movies = $Order->q("select `movie` from `orders` Group by `movie`");
@@ -12,7 +13,8 @@
         }
         ?>
     </select>
-    <button>刪除</button>
+    <button onclick='qdel()'>刪除</button>
+    <!-- 會用以下ajax觸發 連動到api/qdel.php -->
     <style>
         /* 未完成 */
         .lists {
@@ -39,16 +41,14 @@
         }
     </style>
 </div>
-<div class="lists">
-    <div class="header">
-        <div>訂單編號</div>
-        <div>電影名稱</div>
-        <div>日期</div>
-        <div>場次時間</div>
-        <div>訂購數量</div>
-        <div>訂購位置</div>
-        <div>操作</div>
-    </div>
+<div class="header">
+    <div>訂單編號</div>
+    <div>電影名稱</div>
+    <div>日期</div>
+    <div>場次時間</div>
+    <div>訂購數量</div>
+    <div>訂購位置</div>
+    <div>操作</div>
 </div>
 <div class="lists">
     <?php
@@ -86,5 +86,19 @@
         }, () => {
             location.reload();
         })
+    }
+
+    function qdel() {
+        let type = $("input[name='type']:checked").val()
+        let val = $("#" + type).val()
+        let chk = confirm(`你確定要刪除${type}為${val}的所有資料嗎?`)
+        if (chk) {
+            $.post("./api/qdel.php", {
+                type,
+                val
+            }, () => {
+                location.reload();
+            })
+        }
     }
 </script>
